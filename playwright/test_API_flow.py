ad535f36c0d1ca4d21489e50c82026e7e979ebb4
Shuvankar Dash <shuvankar95@gmail.com>
2025-12-07 19:25:44 +0100

    Rest_API testing

--- playwright/test_API_flow.py
+++ playwright/test_API_flow.py
@@ -1,23 +1,14 @@
 from playwright.sync_api import Playwright
 
-from utils.web_base import APIUtils
 
+def test_API_flow (playwright: Playwright):
 
-def test_API_flow(playwright:Playwright):
-
-    #login functionality
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
     page = context.new_page()
-
-    # Create order -> Order Id
-    api_utils = APIUtils()
-    api_utils.create_order(playwright)
-
 
     page.goto("https://rahulshettyacademy.com/client")
 
     page.get_by_placeholder("email@emaxple.com").fill("shuvo@gmail.com")
     page.get_by_placeholder("enter your passsword").fill("Hofbeatles123!")
     page.get_by_role("button", name="Login").click()
-
--- playwright/utils/web_base.py
+++ playwright/utils/web_base.py
@@ -1,30 +1,26 @@
-
 from playwright.sync_api import Playwright
 
 orders_payload= {"orders": [{"country": "British Indian Ocean Territory", "productOrderedId": "68a961459320a140fe1ca57a"}]}
 
 class APIUtils:
 
-    def get_token(self, playwright: Playwright):
-       api_get_token = playwright.request.new_context(base_url="https://rahulshettyacademy.com/client")
-       response = api_get_token.post("/api/ecom/auth/login",
-                                     data={"userEmail": "shuvo@gmail.com", "userPassword": "Hofbeatles123!"})
-
-       assert response.ok
-       print(response.json())
-       response_body = response.json()
-       print(response_body)
-       token = response_body["token"]
-       return token
-
+    def create_token(self, playwright: Playwright):
+        token_request_context = playwright.request.new_context(base_url="https://rahulshettyacademy.com/client")
+        response = token_request_context.post("/api/ecom/auth/login",
+                                   data={"userEmail": "shuvo@gmail.com", "userPassword": "Hofbeatles123!"})
+        assert response.ok
+        print(response.json())
+        response_body = response.json()
+        return response_body["token"]
 
     def create_order(self, playwright:Playwright):
 
-        token = self.get_token(playwright)
+        token = self.create_token(playwright)
+
         api_request_context = playwright.request.new_context(base_url="https://rahulshettyacademy.com/client")
-        response =api_request_context.post("/api/ecom/order-order",
-                                 data=orders_payload,
-                                 headers = {"Authorization": token, "Content-Type": "application/json"}
-        )
+        response = api_request_context.post("/api/ecom/auth/order",
+                                            data=orders_payload,
+                                            headers={"Content-Type": "application/json", "Authorization": token }
+                                            )
 
         print(response.json())

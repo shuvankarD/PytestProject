623ba5ed4d64e18b5addec2d638b42ec879be878
Shuvankar Dash <shuvankar95@gmail.com>
2025-12-30 12:04:13 +0100

    Mock_request

--- playwright/test_network.py
+++ playwright/test_network.py
@@ -1,24 +1,34 @@
+import time
 
 from playwright.sync_api import Page
 
-fake_response = {"data":[],"message":"No Orders"}
+#fake_response = {"data":[],"message":"No Orders"}
 
-def intercept_response(route):
-    route.fulfill(
-        json = fake_response
-    )
+# def intercept_response(route):
+#     route.fulfill(
+#         json=fake_response
+#     )
+def intercept_request(route):
+   route.continue_(url="https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=/6953a7cdc941646b7a706b98")
+
 
 def test_network(page: Page):
 
     page.goto("https://rahulshettyacademy.com/client")
 
-    page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*", intercept_response)
+   #page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*", intercept_response)
+
+    page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id= *", intercept_request)
 
     page.get_by_placeholder("email@example.com").fill("shuvo@gmail.com")
     page.get_by_placeholder("enter your passsword").fill("Hofbeatles123!")
     page.get_by_role("button", name="Login").click()
 
     page.get_by_role("button", name="ORDERS").click()
+    page.get_by_role("button", name="VIEW").first.click()
 
-    order_text = page.locator(".mt-4").text_content()
-    print(order_text)
+    time.sleep(5)
+
+    message = page.locator(".blink_me").text_content()
+    print(message)
+
